d2ec3b5e66d294ab60872aa2f53809da35296d64
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-07 02:37:41 -0600

    Initial Commit

---
+++ FirebaseBootcampST/FirebaseBootcampSTApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBootcampSTApp.swift
+//  FirebaseBootcampST
+//
+//  Created by Mitch Andrade on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseBootcampSTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
