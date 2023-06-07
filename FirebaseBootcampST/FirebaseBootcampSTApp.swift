f827a4f8d0758de5be2eb25b2d131c3506e15d0f
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-07 16:20:10 -0600

    -- Getting Started - Installed Firebase

--- FirebaseBootcampST/FirebaseBootcampSTApp.swift
+++ FirebaseBootcampST/FirebaseBootcampSTApp.swift
@@ -6,12 +6,30 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct FirebaseBootcampSTApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    init() {
+        FirebaseApp.configure()
+        print("Configured Firebase!")
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
 }
